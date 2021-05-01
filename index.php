69b20b647137297881d1f4006ebaa8d06c977fcc
josuemrosario <josuemrosario@yahoo.com.br>
2021-04-30 23:31:56 -0300

    Aula 350 - Autenticando Usuário

--- index.php
+++ index.php
@@ -39,6 +39,20 @@
                 <div class="form-group">
                   <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+
+                <? if(isset($_GET['login'])){ ?>
+                
+                  <div class="text-danger">
+                    Usuário ou senha invalido(s)
+                  </div>
+                
+                <? } ?>
+
+
+                
+
+                
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,25 @@
 <?php
 
 	print_r($_POST);
+	$usuario_autenticado = false;
+
+	//Usuarios do sistema
+	$usuarios_app = array(
+		'josue@yahoo.com.br'=>'1234',
+		'user@teste.com.br'=>'abcd'
+	);
+
+	foreach ($usuarios_app as $user => $senha) {
+		if($_POST['email'] == $user && $_POST['senha'] == $senha) {
+			$usuario_autenticado = true;
+			break;
+		}
+	}	
+
+	if(!($usuario_autenticado)){
+		header('Location: index.php?login=erro');
+	}
+
+	
 
 ?>
